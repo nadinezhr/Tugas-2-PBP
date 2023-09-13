87d8d6260a81ff5db75465174b3d8f812c814303
nadinezhr <dyandrandnzahira@gmail.com>
2023-09-13 10:15:06 +0700

    Memodifikasi README.md dan tests.py

--- main/tests.py
+++ main/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase, Client
+from .models import Item
 
 class mainTest(TestCase):
-    def test_main_url_is_exist(self):
-        response = Client().get('/main/')
-        self.assertEqual(response.status_code, 200)
+    def test_items_displayed_correctly(self):
+        # Buat beberapa objek Item sebagai contoh
+        item1 = Item.objects.create(name='Item 1', price=105)
+        item2 = Item.objects.create(name='Item 2', price=23)
 
-    def test_main_using_main_template(self):
+        # Lakukan permintaan ke halaman 'main'
         response = Client().get('/main/')
-        self.assertTemplateUsed(response, 'main.html')
+
+        # Periksa apakah nama dan harga item-item yang ada sesuai dengan yang diharapkan
+        self.assertContains(response, item1.name)
+        self.assertContains(response, item1.price)
+        self.assertContains(response, item2.name)
+        self.assertContains(response, item2.price)

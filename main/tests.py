668ef82e030bacea8dd52db22a036b37251a7548
nadinezhr <dyandrandnzahira@gmail.com>
2023-09-13 10:43:29 +0700

    Memodifikasi tests.py

--- main/tests.py
+++ main/tests.py
@@ -2,16 +2,7 @@ from django.test import TestCase, Client
 from .models import Item
 
 class mainTest(TestCase):
-    def test_items_displayed_correctly(self):
-        # Buat beberapa objek Item sebagai contoh
-        item1 = Item.objects.create(name='Item 1', price=105)
-        item2 = Item.objects.create(name='Item 2', price=23)
-
-        # Lakukan permintaan ke halaman 'main'
+    def test_main_contains_expected_content(self): #Testing apakah tulisan ada dalam konten web
         response = Client().get('/main/')
-
-        # Periksa apakah nama dan harga item-item yang ada sesuai dengan yang diharapkan
-        self.assertContains(response, item1.name)
-        self.assertContains(response, item1.price)
-        self.assertContains(response, item2.name)
-        self.assertContains(response, item2.price)
+        self.assertContains(response, 'Selamat datang di Nadterial Store')
+        
